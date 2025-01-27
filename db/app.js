f45cae72bb1e37935f7c20647e83be1ea6fbab01
alexiskelsall <alexis@LexiK.>
2025-01-27 15:25:30 +0000

    add get/api/topics

--- db/app.js
+++ db/app.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const app = express ()
 const endpoints = require("../endpoints.json");
+const {getTopics} = require("./Controllers/get.controllers")
 
-
+app.use(express.json())
 
 app.get('/api', (req, res)=>{
     res.status(200).send({endpoints})
+})
+
+
+app.get('/api/topics', getTopics)
+
+app.all("*",(req, res)=>{
+    res.status(404).send({error:"Endpoint not found"})
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err)
+    res.status(500).send({msg:"Server Error!"})
 })
 
 module.exports = app
